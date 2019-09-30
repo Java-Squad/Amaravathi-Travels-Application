68242b76677c26fffb3d28e99ae743365b6eb7c4
Jasti Satyakrishna <satyajasti9@gmail.com>
2019-09-30 20:40:42 +0530

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date.*;
+public class Date {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Date d=new Date();
+		SimpleDateFormat s=new SimpleDateFormat("E dd-mm-yyy hh:mm a");
+		System.out.println(d);
+    }
+    }
